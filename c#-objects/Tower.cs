38e6466d353ee41929935f4df41ccfa443c89589
Chris Dehner <topherdehner@gmail.com>
2020-09-17 19:24:15 -0400

    Initial commit

---
+++ c#-objects/Tower.cs
@@ -0,0 +1,10 @@
+namespace TreehouseDefenese
+{
+	class Tower
+	{
+		public void Main()
+		{
+			Tower tower = new Tower();
+		}
+	}
+}
---
+++ omnisharp-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace omnisharp_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		Console.WriteLine("Hello, world!");
+	}
+    }
+}
