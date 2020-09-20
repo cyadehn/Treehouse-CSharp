f7bf08d641b0a6ae955fd18a05c8a7060f01773f
Chris Dehner <topherdehner@gmail.com>
2020-09-20 02:39:16 -0400

    Feat: Add check for whether invaders have won

--- c#-objects/Invader.cs
+++ c#-objects/Invader.cs
@@ -7,9 +7,16 @@ namespace TreehouseDefense
       
       public MapLocation Location => _path.GetLocationAt(_pathStep);
       
-      //Setter made private to make clear they should use method below to influence the health
+      //
+      //Setter made private
+      //User should use DecreaseHealth(factor)
       public int Health { get; private set; } = 100;
-    
+      //
+      
+      // True if the invader has reached the end of the path
+      public bool HasScored() => _pathStep >= _path.Length; 
+
+	public bool IsNeutralized() => (Health <= 0) ? true : false;
       public Invader(Path path)
       {
           _path = path;
