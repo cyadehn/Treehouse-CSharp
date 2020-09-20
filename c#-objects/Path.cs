9ddb983940d144ad8adf68dd8bd36ff62834bbfe
Chris Dehner <topherdehner@gmail.com>
2020-09-20 03:52:35 -0400

    Chore: Refactor for readability

--- c#-objects/Path.cs
+++ c#-objects/Path.cs
@@ -2,18 +2,23 @@ namespace TreehouseDefense
 {
 	class Path
 	{
-      private readonly MapLocation[] _path;
-    
-      public int Length => _path.Length;
+		private readonly MapLocation[] _path;
 
-      public Path(MapLocation[] path)
-      {
-          _path = path; 
-      }
-    
-      public MapLocation GetLocationAt(int pathStep)
-      {
-          return (pathStep < _path.Length) ? _path[pathStep] : null;
-      }
+		public int Length => _path.Length;
+
+		public Path(MapLocation[] path)
+		{
+		  _path = path; 
+		}
+
+		public MapLocation GetLocationAt(int pathStep)
+		{
+		  return (pathStep < _path.Length) ? _path[pathStep] : null;
+		}
+		
+		public bool OnPath(MapLocation location)
+		{
+			return location.Y == _path[0].Y ? true : false;
+		}
 	}
 }
