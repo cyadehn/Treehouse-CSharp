2251544b736688a9ad9c2525169db05e0eb18630
Chris Dehner <topherdehner@gmail.com>
2020-09-22 18:45:32 -0400

    Fix: Pass correct variables into Tower constructor

--- c#-objects/Game.cs
+++ c#-objects/Game.cs
@@ -7,31 +7,56 @@ namespace TreehouseDefense
 		{
 			Map map = new Map(8, 5);
       
-      try {
-          Path path = new Path(
-            new [] {
-                new MapLocation(0,2,map),
-                new MapLocation(1,2,map),
-                new MapLocation(2,2,map),
-                new MapLocation(3,2,map),
-                new MapLocation(4,2,map),
-                new MapLocation(5,2,map),
-                new MapLocation(6,2,map),
-                new MapLocation(7,2,map),
-            }
-          );
-      }
-      catch (OutOfBoundsException ex)
-      {
-          Console.WriteLine(ex);
-      }
-      catch (TreehouseDefenseException)
-      {
-          Console.WriteLine("Unhandled TreehouseDefenseException");
-      }
-      catch (Exception ex) {
-          Console.WriteLine("Unhandled Exception: " + ex);
-      }
+			try {
+			  Path path = new Path(
+			    new [] {
+				new MapLocation(0,2,map),
+				new MapLocation(1,2,map),
+				new MapLocation(2,2,map),
+				new MapLocation(3,2,map),
+				new MapLocation(4,2,map),
+				new MapLocation(5,2,map),
+				new MapLocation(6,2,map),
+				new MapLocation(7,2,map),
+			    }
+			  );
+
+			  Invader[] invaders =
+			  {
+				  new Invader(path),
+				  new Invader(path),
+				  new Invader(path),
+				  new Invader(path)
+			  };
+
+			  Tower[] towers =
+			  {
+				  new Tower(new MapLocation(1, 3, map), path, 2, 10),
+				  new Tower(new MapLocation(1, 3, map), path, 3, 5),
+				  new Tower(new MapLocation(1, 3, map), path, 1, 20),
+			  };
+
+			  Level level = new Level(invaders)
+			  {
+				  Towers = towers
+			  };
+
+			  bool playerWon = level.Play();
+
+			  Console.WriteLine($"Player " + (playerWon? "won" : "lost"));
+			}
+
+			catch (OutOfBoundsException ex)
+			{
+			  Console.WriteLine(ex);
+			}
+			catch (TreehouseDefenseException)
+			{
+			  Console.WriteLine("Unhandled TreehouseDefenseException");
+			}
+			catch (Exception ex) {
+			  Console.WriteLine("Unhandled Exception: " + ex);
+			}
 		}
 	}
 }
