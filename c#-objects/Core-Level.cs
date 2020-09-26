14470680babc39fe554024b233f40615be4931f4
Chris Dehner <topherdehner@gmail.com>
2020-09-26 02:11:20 -0400

    Chore: clean up indents

--- c#-objects/Core-Level.cs
+++ c#-objects/Core-Level.cs
@@ -1,51 +1,55 @@
 namespace TreehouseDefense
 {
-	class Level
-	{
-		private readonly IInvader[] _invaders;
+    class Level
+    {
+        private readonly IInvader[] _invaders;
 
-		// Currently set on Level init in Game.cs
-		public Tower[] Towers { get; set; }
+        // Currently set on Level init in Game.cs
+        public Tower[] Towers { get; set; }
 
-		public Level(IInvader[] invaders)
-		{
-			_invaders = invaders;
-		}
-		
-		//Returns: true if the player wins
-		//False otherwise
-		public bool Play()
-		{
-			// Run until all invaders are neutralized or an invader reaches the end
-			int remainingInvaders = _invaders.Length;
+        public Level(IInvader[] invaders)
+        {
+            _invaders = invaders;
+        }
 
-			while( remainingInvaders > 0 )
-			{
-				// Each tower has opportunity to fire on invaders
-				foreach( Tower tower in Towers )
-				{
-					tower.FireOnInvaders(_invaders);
-				}
+        //Returns: true if the player wins
+        //False otherwise
+        public bool Play()
+        {
+            // Run until all invaders are neutralized or an invader reaches the end
+            int remainingInvaders = _invaders.Length;
 
-				// Count and move the invaders that are still active
-				remainingInvaders = 0;
-				foreach( IInvader invader in _invaders )
-				{
-					if( invader.IsActive )
-					{
-						invader.Move();
+            while( remainingInvaders > 0 )
+            {
+                // Each tower has opportunity to fire on invaders
+                foreach( Tower tower in Towers )
+                {
+                    tower.FireOnInvaders(_invaders);
+                }
 
-						if( invader.HasScored )
-						{
-							return false;
-						}
+                // Count and move the invaders that are still active
+                remainingInvaders = 0;
+                foreach( IInvader invader in _invaders )
+                {
+                    if( invader.IsActive )
+                    {
+                        invader.Move();
 
-						remainingInvaders++;
-					}
-				}
-			}
+                        if( invader.HasScored )
+                        {
+                            return false;
+                        }
+                        
+                        if ( invader.IsArmed ) 
+                        {
+                            invader.FireOnTowers();
+                        }
+                        remainingInvaders++;
+                    }
+                }
+            }
 
-			return true;
-		}
-	}
+            return true;
+        }
+    }
 }
