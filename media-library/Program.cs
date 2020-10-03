da021681e71ecd5c09d022d178aaeeba80ee89c8
Chris Dehner <topherdehner@gmail.com>
2020-10-02 20:46:19 -0400

    Feat: Initialize array of media items

--- media-library/Program.cs
+++ media-library/Program.cs
@@ -25,19 +25,22 @@ namespace Treehouse.MediaLibrary
         static void Main()
         {
             try {
-                VideoGame game1 = new VideoGame("The Legend of Zelda: Ocarina of Time", "November 21, 1998", "Nintendo EAD", "Nintendo", "Nintendo 64");
-                VideoGame game2 = new VideoGame("Megaman Battle Network", "March 21, 2001", "Capcom", "Capcom", "GameBoy Advance");
-                VideoGame game3 = new VideoGame("Billy Hatcher and the Giant Egg", "September 23, 2003", "Sonic Team", "Sega", "Nintendo Gamecube");
 
-                Film film1 = new Film("Harold and Maude", 1971, "Hal Ashby");
-                Film film2 = new Film("Me and Earl and the Dying Girl", 2013, "Alfonzo Gomez-Rejon");
-                Film film3 = new Film("The Little Prince", 2016, "Mark Osborne");
+                MediaItem[] mediaItems = new MediaItem[] {
+                    VideoGame game1 = new VideoGame("The Legend of Zelda: Ocarina of Time", "November 21, 1998", "Nintendo EAD", "Nintendo", "Nintendo 64");
+                    VideoGame game2 = new VideoGame("Megaman Battle Network", "March 21, 2001", "Capcom", "Capcom", "GameBoy Advance");
+                    VideoGame game3 = new VideoGame("Billy Hatcher and the Giant Egg", "September 23, 2003", "Sonic Team", "Sega", "Nintendo Gamecube");
 
-                Composition composition1 = new Composition("Symphony No. 5, Op. 67", "Ludwig van Beethoven", 1808);          
-                Composition composition2 = new Composition("Hello World", "Louie Zong", 2018);
-                Composition composition3 = new Composition("Just A Quail", "Louie Zong", 2019);
-                Composition composition4 = new Composition("Bird With A Broken Wing", "Adam Young", 2015);
-                Composition composition5 = new Composition("Deer In The Headlights", "Owl City", 2011);
+                    Film film1 = new Film("Harold and Maude", 1971, "Hal Ashby");
+                    Film film2 = new Film("Me and Earl and the Dying Girl", 2013, "Alfonzo Gomez-Rejon");
+                    Film film3 = new Film("The Little Prince", 2016, "Mark Osborne");
+
+                    Composition composition1 = new Composition("Symphony No. 5, Op. 67", "Ludwig van Beethoven", 1808);
+                    Composition composition2 = new Composition("Hello World", "Louie Zong", 2018);
+                    Composition composition3 = new Composition("Just A Quail", "Louie Zong", 2019);
+                    Composition composition4 = new Composition("Bird With A Broken Wing", "Adam Young", 2015);
+                    Composition composition5 = new Composition("Deer In The Headlights", "Owl City", 2011);
+                }
 
                 DetectMediaType(game1);
                 DetectMediaType(film1);
