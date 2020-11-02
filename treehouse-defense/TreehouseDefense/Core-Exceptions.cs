11f0700757c02e0462cf8ac089531442fb31db05
Chris Dehner <topherdehner@gmail.com>
2020-11-02 00:11:51 -0500

    Chore: Correct indenting for Vim folds

--- treehouse-defense/TreehouseDefense/Core-Exceptions.cs
+++ treehouse-defense/TreehouseDefense/Core-Exceptions.cs
@@ -3,29 +3,22 @@ namespace TreehouseDefense
     class TreehouseDefenseException : System.Exception 
     {
         public TreehouseDefenseException()
-        {
-        }
+        {}
         public TreehouseDefenseException(string message) : base(message)
-        {
-        }
+        {}
     }
-  
     class  OutOfBoundsException : TreehouseDefenseException 
     {
         public OutOfBoundsException()
-        {
-        }
+        {}
         public OutOfBoundsException(string message) : base(message)
-        { 
-        }
+        {}
     }
     class OnPathException : TreehouseDefenseException
 	{
 		public OnPathException()
-		{
-		}
+		{}
 		public OnPathException(string message) : base(message)
-		{
-		}
+		{}
 	}
 }
