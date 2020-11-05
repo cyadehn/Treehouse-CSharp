9da7219fc1d924cd935f3c22cd49949736566697
Chris Dehner <topherdehner@gmail.com>
2020-11-05 18:36:17 -0500

    Update test files for course tests

--- treehouse-defense/TreehouseDefense.Tests/PointTests.cs
+++ treehouse-defense/TreehouseDefense.Tests/PointTests.cs
@@ -1,51 +1,87 @@
-using Xunit;
-using static TreehouseDefense.Point;
+﻿using Xunit;
 
 namespace TreehouseDefense.Tests
 {
     public class PointTests
     {
-        [Fact()]
+        [Fact]
         public void PointTest()
         {
-            // prepare
             int x = 5;
             int y = 6;
-            //act
-            var point = new Point(x,y);
-            //assert
-            Assert.Equal(x,point.X);
-            Assert.Equal(y,point.Y);
+
+            var point = new Point(x, y);
+
+            Assert.Equal(x, point.X);
+            Assert.Equal(y, point.Y);
         }
+
         [Fact()]
-        public void DistanceToWithPythagoreanTriple()
+        public void DistanceToWithPathagoreanTriple()
         {
-            var point = new Point(3,4);
-            // "target" since this is the class instance being tested
-            var target = new Point(0,0);
+            var point = new Point(3, 4);
+            var target = new Point(0, 0);
+
             var expected = 5.0;
+
             var actual = target.DistanceTo(point);
-            // third parameter will set the level of precision -- due to floating point comparison here
+
             Assert.Equal(expected, actual, 2);
         }
-        [Fact()]
+
+        [Fact]
         public void DistanceToPointAtSamePosition()
         {
-            var point = new Point(3,4);
-            var target = new Point(3,4);
+            var point = new Point(3, 4);
+            var target = new Point(3, 4);
+
             var expected = 0.0;
+
             var actual = target.DistanceTo(point);
+
             Assert.Equal(expected, actual, 2);
-
         }
-        [Fact()]
-        public void EqualsWithDifferentInstances()
-        {
-            var point1 = new Point(3,4);
-            var point2 = new Point(3,4);
-            bool result = point1.Equals(point2);
-            Assert.True(result);
 
+        [Fact]
+        public void DoesNotEqualNull()
+        {
+            var target = new Point(0, 0);
+            Assert.False(target.Equals(null));
+        }
+
+        [Fact]
+        public void DistinctObjectsAreEqual()
+        {
+            var target = new Point(4, 5);
+            Assert.True(target.Equals(new Point(4, 5)));
+        }
+
+        [Fact]
+        public void PointsAreNotEqual()
+        {
+            var target = new Point(4, 5);
+            Assert.False(target.Equals(new Point(4, 6)));
+        }
+
+        [Fact]
+        public void SimilarPointsHaveDifferentHashCodes1()
+        {
+            var target = new Point(4, 5);
+            Assert.NotEqual(new Point(5, 4).GetHashCode(), target.GetHashCode());
+        }
+
+        [Fact]
+        public void SimilarPointsHaveDifferentHashCodes2()
+        {
+            var target = new Point(4, 5);
+            Assert.NotEqual(new Point(4, 6).GetHashCode(), target.GetHashCode());
+        }
+
+        [Fact]
+        public void EqualPointsHaveSameHashCodes()
+        {
+            var target = new Point(4, 5);
+            Assert.Equal(new Point(4, 5).GetHashCode(), target.GetHashCode());
         }
     }
 }
